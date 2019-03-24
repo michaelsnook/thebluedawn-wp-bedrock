ab6ddbe69e770b5f4d3b4b3c7767f7a8f54e8a0a
Michael Snook <snook@the-open.net>
2019-03-24 20:44:49 +0530

    Clean up banner/header/jumbotron

--- web/app/themes/thebluedawn/resources/views/home.blade.php
+++ web/app/themes/thebluedawn/resources/views/home.blade.php
@@ -1,23 +1,13 @@
 @extends('layouts.app')
 
-<div class="jumbotron mb-0 bg-primary text-primary">
-  <div class="container p-4">
-    <h1 class="display-4">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    <a class="btn btn-outline-primary btn-lg" href="#" role="button">Learn more</a>
-  </div>
-</div>
-
 @section('content')
 
   @include('partials.page-header')
+
   @if (!have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, no results were found.', 'sage') }}
     </div>
-    {!! get_search_form(false) !!}
   @endif
 
   @while (have_posts()) @php the_post() @endphp
--- web/app/themes/thebluedawn/resources/views/partials/header.blade.php
+++ web/app/themes/thebluedawn/resources/views/partials/header.blade.php
@@ -1,18 +1,38 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-  <div class="container">
-    <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu([
-          'theme_location' => 'primary_navigation',
-          'container' => '',
-          'menu_class' => 'navbar-nav ml-auto',
-          'walker' => new WP_Bootstrap_Navwalker(),
-          ]) !!}
-      @endif
+<section class="header mb-5">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu([
+            'theme_location' => 'primary_navigation',
+            'container' => '',
+            'menu_class' => 'navbar-nav ml-auto',
+            'walker' => new WP_Bootstrap_Navwalker(),
+            ]) !!}
+        @endif
+      </div>
+    </div>
+  </nav>
+
+@if ( is_front_page() )
+  <div class="jumbotron mb-0 bg-primary text-white">
+    <div class="container p-5">
+      <h1 class="display-4">Welcome to The Blue Dawn</h1>
+      <p class="lead">
+        Blue Dawn is a community initiative/support group working by values of community healing and a facilitator of accessible mental health services to Bahujans.
+      </p>
+      <p>
+        We connect Bahujan people in need of mental health counseling with counselors who understand the intersection
+        of mental health and caste/minority issues, and with sponsors who can help make mental health counseling accessible to our community.
+      </p>
+      <a class="btn btn-light btn-lg" href="#" role="button">Seek Counseling</a>
+      <a class="btn btn-outline-light btn-lg" href="#" role="button">Sponsor Counseling</a>
     </div>
   </div>
-</nav>
+@endif
+
+</section>
