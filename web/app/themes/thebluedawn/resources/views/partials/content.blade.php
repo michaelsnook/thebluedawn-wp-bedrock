551d59caf578109f7f45b67dea872d8ef2749e1b
Michael Snook <snook@the-open.net>
2019-03-25 23:59:33 +0530

    Add featured image to list view and post view

--- web/app/themes/thebluedawn/resources/views/partials/content.blade.php
+++ web/app/themes/thebluedawn/resources/views/partials/content.blade.php
@@ -1,9 +1,20 @@
-<article @php post_class("text-muted pt-3 border-bottom border-gray") @endphp>
-  <header>
-    <h4 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h4>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
+<article @php post_class("text-muted pt-3 border-bottom border-gray clearfix") @endphp>
+  <h4 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h4>
+  <div class="row">
+    @if ( has_post_thumbnail() )
+    <div class="col-md-3 col-12 pb-3">
+      <a href="{{ get_permalink() }}">
+        {!! the_post_thumbnail('large', ['class' => 'rounded img-fluid']) !!}
+      </a>
+    </div>
+    @endif
+    <div class="mx-3">
+      <header>
+        @include('partials/entry-meta')
+      </header>
+      <div class="entry-summary">
+        @php the_excerpt() @endphp
+      </div>
+    </div>
   </div>
 </article>
