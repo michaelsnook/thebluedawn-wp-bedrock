3d565af17b462e39b5bca3b0b958b7ab8a931c24
Michael Snook <snook@the-open.net>
2019-03-25 20:13:56 +0530

    Add scss components for Header, Jumbotron, Button-1 and -2

--- web/app/themes/thebluedawn/resources/views/partials/header.blade.php
+++ web/app/themes/thebluedawn/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<section class="header">
+<header class="Header">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
       <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
@@ -19,7 +19,7 @@
   </nav>
 
 @if ( is_front_page() )
-  <div class="jumbotron mb-0 bg-primary text-white">
+  <div class="Jumbotron">
     <div class="container p-5">
       <h1 class="display-4">Welcome to The Blue Dawn</h1>
       <p class="lead">
@@ -29,10 +29,10 @@
         We connect Bahujan people in need of mental health counseling with counselors who understand the intersection
         of mental health and caste/minority issues, and with sponsors who can help make mental health counseling accessible to our community.
       </p>
-      <a class="btn btn-light btn-lg" href="#" role="button">Seek Counseling</a>
-      <a class="btn btn-outline-light btn-lg" href="#" role="button">Sponsor Counseling</a>
+      <a class="Button-1" href="#" role="button">Seek Counseling</a>
+      <a class="Button-2" href="#" role="button">Sponsor Counseling</a>
     </div>
   </div>
 @endif
 
-</section>
+</header>
