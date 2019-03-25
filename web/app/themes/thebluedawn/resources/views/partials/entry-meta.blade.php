41f88c0b695c240be724c55ba6e10a89e3f8abc6
Michael Snook <snook@the-open.net>
2019-03-26 00:00:22 +0530

    Linkify timestamp

--- web/app/themes/thebluedawn/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/thebluedawn/resources/views/partials/entry-meta.blade.php
@@ -1,5 +1,7 @@
 <h6 class="text-muted">
-  <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+  <a href="{{ get_permalink() }}">
+    <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+  </a>
   <span class="byline author vcard">
     {{ __('by', 'sage') }}
     <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
