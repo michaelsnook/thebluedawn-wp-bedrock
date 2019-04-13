e676595bd6ba1cb137dc53c41424c387dfe9f75b
Michael Snook <snook@the-open.net>
2019-04-13 23:03:39 +0530

    Add fontawesome social icons

--- web/app/themes/thebluedawn/resources/views/partials/footer.blade.php
+++ web/app/themes/thebluedawn/resources/views/partials/footer.blade.php
@@ -2,9 +2,48 @@
   <div class="container">
     <div class="row">
       @php dynamic_sidebar('sidebar-footer') @endphp
+      <section class="widget nav_menu-2 widget_nav_menu">
+        <h3>Follow Us</h3>
+        <div class="menu-social-media-menu-container">
+          <ul id="menu-social-media-menu" class="menu">
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-23">
+              <a href="https://twitter.com/thebluedawn56">
+                <i class="fab fa-twitter"></i>
+                Twitter
+              </a>
+            </li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-24">
+              <a href="https://www.instagram.com/thebluedawn56/">
+                <i class="fab fa-instagram"></i>
+                Instagram
+              </a>
+            </li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-22">
+              <a href="https://www.facebook.com/thebluedawn56/">
+                <i class="fab fa-facebook"></i>
+                Facebook
+              </a>
+            </li>
+            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-25">
+              <a href="http://https://medium.com/@thebluedawnwrites/">
+                <i class="fab fa-medium"></i>
+                Medium
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
       <div class="col">
         <img src="/app/themes/thebluedawn/dist/images/logo-inverted.png" class="img-fluid">
       </div>
+    </div>
+
+    <div class="row text-center p-3 mt-3">
+      <p>
+        Copyright &copy; The Blue Dawn 2019.
+        This site and WordPress theme are <a class="text-secondary" href="https://github.com/michaelsnook/thebluedawn-wp-bedrock/">available on GitHub</a> to use and adapt under an MIT license.
+        Made with WordPress, Bedrock, Timber, and Bootstrap, by <a class="text-secondary" href="https://github.com/michaelsnook">Michael Snook</a>.
+      </p>
     </div>
   </div>
 </footer>
