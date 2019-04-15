5c5414ffd19b90f5199e08b50b9b39b0a31409f7
Michael Snook <snook@the-open.net>
2019-04-15 17:59:43 +0530

    Restyle home page cards into card-post.twig

--- web/app/themes/thebluedawn/resources/views/home.php
+++ web/app/themes/thebluedawn/resources/views/home.php
@@ -22,6 +22,46 @@ $context['testimonials'] = new Timber\PostQuery(array(
 $context['press'] = new Timber\PostQuery(array(
   'category_name' => 'media'
 ));
+$wwd = array();
+$wwd[0] = array(
+    'thumbnail' => array(
+      'src' => '//thebluedawn.org/app/uploads/2019/04/IMG1_page-0001-1.jpg',
+      'alt' => 'Painting of two trees'
+    ),
+    'title' => 'Matching clients with counselors',
+    'excerpt' => 'Blue Dawn facilitates anti-caste, queer-friendly, disabled-friendly mental health care services to Bahujans. If you are a Bahujan and seeking support, please fill the Google Form to get in touch with us.',
+    'cta' => array(
+      'href' => '/speak-to-a-counselor/',
+      'text' => 'Get started'
+    )
+  );
+
+$wwd[1] = array(
+    'thumbnail' => array(
+      'src' => '//thebluedawn.org/app/uploads/2019/04/IMG_page-0001-1.jpg',
+      'alt' => 'Painting of birds sitting in a tree that grew out from a cage'
+    ),
+    'title' => 'Network of Bahujan-friendly counselors',
+    'excerpt' => 'If you are a counselor or psychiatrist who understands structural oppression, especially caste oppression, and are interested in joining our network, please fill the Google Form to get in touch.',
+    'cta' => array(
+      'href' => '/counselors-network/',
+      'text' => 'Get involved'
+    )
+  );
+$wwd[2] = array(
+    'thumbnail' => array(
+      'src' => '//thebluedawn.org/app/uploads/2019/04/IMG_0001_page-0001-1.jpg',
+      'alt' => 'Painting of two people helping each other climb a mountain'
+    ),
+    'title' => 'Sponsor clients in need',
+    'excerpt' => 'If you are neither a counselor nor a patient but want to make mental health care accessible to all, you can sponsor therapy sessions for clients in need. Please fill the Google Form to get in touch with us.',
+    'cta' => array(
+      'href' => '/sponsor-sessions/',
+      'text' => 'Get in touch'
+    )
+  );
+
+$context['what_we_do'] = $wwd;
 
 // $whatwedo = new Timber\Post(array( 'pagename' => 'what-we-do' ));
 // $context['whatwedo_pages'] = $whatwedo->children;
